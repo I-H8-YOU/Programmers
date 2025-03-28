f1bc3269828442dba036bf5b9bf378372925b25e
Jay <103248677+I-H8-YOU@users.noreply.github.com>
2025-03-28 09:22:33 +0900

    [level 0] Title: 수 조작하기 1, Time: 64.18 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        String[] sArr = control.split("");
+        for(int i = 0 ; i < sArr.length ; i++){
+            switch(sArr[i]){
+                case "w" : n += 1;
+                    break;
+                case "s" : n -= 1;
+                    break;
+                case "d" : n += 10;
+                    break;
+                case "a" : n -= 10;
+                    break;
+                default : n += 0;
+            }
+            answer = n;
+        }
+        return answer;
+    }
+}
