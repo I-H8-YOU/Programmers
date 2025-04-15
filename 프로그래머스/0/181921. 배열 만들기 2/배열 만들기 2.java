7ea25b20b309220ee0ae24f8f2c4b5a592e1c2d5
Jay <103248677+I-H8-YOU@users.noreply.github.com>
2025-04-15 10:55:08 +0900

    [level 0] Title: 배열 만들기 2, Time: 439.47 ms, Memory: 382 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int l, int r) {
+        int[] answer = {};
+        String chk = "1,2,3,4,6,7,8,9";
+        String[] sList = {"1","2","3","4","6","7","8","9"};
+            answer = IntStream.rangeClosed(l,r).map(m -> {
+            String s = Integer.toString(m);
+            if(s.matches("[05]+")) return Integer.parseInt(s);
+            else return 0;
+        }).filter(f -> f > 0).toArray();
+        if(answer.length == 0){
+            answer = new int[]{-1};
+        }
+        return answer;
+    }
+}
