b9096bd4e017eaffa40870b5f06327081e07bd82
Jay <103248677+I-H8-YOU@users.noreply.github.com>
2025-04-26 22:53:59 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.81 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        int range = right - left;
+        int copyLeft = left;
+        int cnt = 0;
+        List<Integer> nList = new ArrayList<>();
+        for(int i = 0 ; i < range + 1 ; i++){
+            cnt = 0;
+            for(int k = 1 ; k < copyLeft ; k++ ){
+                if(copyLeft % k == 0){
+                    cnt++;
+                }
+            }
+            copyLeft++;
+            nList.add(cnt+1);
+        }
+        int idx = 0;
+        for(int m = left ; m <= right ; m++){
+            if(nList.get(idx) % 2 == 0){
+                answer += m;
+                idx++;
+            }else{
+                answer -= m;
+                idx++;
+            }
+        }
+        return answer;
+    }
+}
