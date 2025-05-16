9cd9203ac18265788440abfd386b2257f64f4180
Jay <103248677+I-H8-YOU@users.noreply.github.com>
2025-05-16 09:57:53 +0900

    [Bronze V] Title: 사분면 고르기, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        sc.close();
+        int res = 0;
+        if(x > 0 && y > 0){
+            res = 1;
+        }else if(x < 0 && y > 0){
+            res = 2;
+        }else if(x < 0 && y < 0){
+            res = 3;
+        }else{
+            res = 4;
+        }
+        System.out.println(res);
+    }
+}
